c8e5881e3c0dc7a7b9580a050eb4a0ad756f2ac4
sylvielcq <slcq.dev@gmail.com>
2022-08-04 01:49:50 -0700

    added spaces for readability

--- breedFetcher.js
+++ breedFetcher.js
@@ -7,18 +7,22 @@ const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
 
 request(URL, (error, response, body) => {
+  
   if (error) {
     console.log('Error: something went wrong');
   }
+
   if (!error) {
+
     if (!breed) {
       console.log('Error: Breed name not found');
     }
-  
+
     if (breed) {
       const data = JSON.parse(body);
   
       console.log(data[0].description);
     }
   }
+  
 });
