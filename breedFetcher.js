8f79c44e1b5bf11124c781509ee30c2215f75c5d
sylvielcq <slcq.dev@gmail.com>
2022-08-04 01:48:05 -0700

    first commit

---
+++ breedFetcher.js
@@ -0,0 +1,24 @@
+
+const arg = process.argv;
+const breed = arg[2];
+
+const request = require('request');
+const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+
+request(URL, (error, response, body) => {
+  if (error) {
+    console.log('Error: something went wrong');
+  }
+  if (!error) {
+    if (!breed) {
+      console.log('Error: Breed name not found');
+    }
+  
+    if (breed) {
+      const data = JSON.parse(body);
+  
+      console.log(data[0].description);
+    }
+  }
+});
