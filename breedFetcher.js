81b4c8f8bbbc96cc2a36a4324e39e0ebcc86e062
sylvielcq <slcq.dev@gmail.com>
2022-08-04 13:29:51 -0700

    Refactored and added tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,39 @@
-
-const arg = process.argv;
-const breed = arg[2];
-
 const request = require('request');
-const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
 
-request(URL, (error, response, body) => {
+// Fetch the breed description from the cats API or return an error.
+// The fetBreedDescription function uses request (= asynchronous), therefore it also
+// needs to be asynchronous by accepting a callback that will return data.
+// The callback is defined in index.js
+
+const fetchBreedDescription = function(breed, callback) {
   
-  if (error) {
-    console.log('Error: something went wrong');
-  }
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  
+  request(URL, (error, response, body) => {
+  
+    // If there is an error during the request to the API (ex: URL is compromised),
+    // call the callback with an error.
+    if (error) {
+      callback(error);
+      return;
+    }
+  
+    // Connection was successfuly made with the API, we're fetching data
+    const data = JSON.parse(body);
 
-  if (!error) {
-
-    if (!breed) {
-      console.log('Error: Breed name not found');
+    // If the data is empty (ex: the breed given doesn't exist, or no breed was given),
+    // call the callback with an error.
+    if (!data.length) {
+      callback('Breed name not found');
+      return;
     }
 
-    if (breed) {
-      const data = JSON.parse(body);
-  
-      console.log(data[0].description);
-    }
-  }
-  
-});
+    // If data was successfully found and fetched,
+    // call the callback with null (as there is no error), and the breed description.
+    callback(null, data[0].description);
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,13 @@
+// This index file is the one users can run and provide the breed name to.
+
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
